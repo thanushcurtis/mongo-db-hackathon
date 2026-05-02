e58edded2db788dc5960e54befdd4f2548d7fa55
BRINDA <brindapragashkumar@gmail.com>
2026-05-02 15:13:12 +0100

    remove backup

--- backup /backupbackup/portfolio-ui/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PortfolioDashboard from './PortfolioDashboard';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PortfolioDashboard />
-  </React.StrictMode>,
-);
